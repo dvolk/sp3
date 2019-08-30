4026e853af178057741aea76dcaad22b6dbbb6ce
Denis Volk <denis.volk@gmail.com>
2019-08-30 10:05:42 +0000

    refactor resistanceapi

--- resistance/resistanceapi/docker/pullAndRunDocker.sh
+++
@@ -1,7 +0,0 @@
-dockerImageName=oxfordmmm/resistanceapi
-dockerpid=`docker ps -a | grep $dockerImageName | grep "Up" | awk -F " " '{ print $1 }'`
-if [[ $dockerpid != "" ]];then
-   docker kill $dockerpid
-fi
-docker pull $dockerImageName
-docker run  -v ~/DataFromDocker/Resistance/vcfs:/vcfs -v ~/DataFromDocker/Resistance/logs:/logs -d -p 8989:8989 --rm $dockerImageName
--- resistance/resistanceapi/setup.sh
+++
@@ -1,3 +0,0 @@
-git clone https://gitlab.com/MMMCloudPipeline/resistanceapi.git
-cd resistanceapi
-pip3 install -r requirements.txt
