db5f68a261d63be7ae8855afa697c8ba7a767931
Denis Volk <denis.volk@gmail.com>
2019-09-03 14:12:14 +0000

    move resistance api config

--- resistance/resistanceapi/api.py
+++ resistance/resistanceapi/api.py
@@ -1,29 +1,38 @@
 import json
+import sys
+import traceback
+import pathlib
 
+import yaml
 import flask
 
 import piezo_resistance
 
 app = flask.Flask(__name__)
 
-resistanceConfig = { 'genbank_file': "/data/reports/resistance/data/H37rV_v3.gbk",
-                     'catalogue_file': "/data/reports/resistance/data/CRYPTICv1.0-RSU-catalogue-H37rV_v3.csv",
-                     'catalogue_name': "CRYPTICv1.0",
-                     'resistance_log':  "/logs/piezo-" }
+resistanceConfig = yaml.load(open('config.yaml', 'r'))
+
+def get_resistance_for_tb_sample(vcf_id):
+    vcf_filename = str(pathlib.Path(resistanceConfig['vcf_location']) / f"{vcf_id}.vcf")
+
+    try:
+        rs = piezo_resistance.get_resistance_for_tb_sample(resistanceConfig['genbank_file'],
+                                                           resistanceConfig['catalogue_file'],
+                                                           resistanceConfig['catalogue_name'],
+                                                           resistanceConfig['resistance_log'],
+                                                           vcf_id,
+                                                           vcf_filename)
+        return json.dumps({ 'status': 'success',
+                            'data': rs,
+                            'message': '' })
+    except Exception as e:
+        return json.dumps({ 'status': 'failure',
+                            'data': '',
+                            'message': traceback.format_exc() })
 
 @app.route('/api/v1/resistances/piezo/<vcf_id>')
-def get_resistance_for_tb_sample(vcf_id):
-    vcf_filename = f"/work/reports/resistanceapi/vcfs/{vcf_id}.vcf"
-
-    rs = piezo_resistance.get_resistance_for_tb_sample(resistanceConfig['genbank_file'],
-                                                       resistanceConfig['catalogue_file'],
-                                                       resistanceConfig['catalogue_name'],
-                                                       resistanceConfig['resistance_log'],
-                                                       vcf_id,
-                                                       vcf_filename)
-    return json.dumps({ 'status': 'success',
-                        'data': rs,
-                        'message': '' })
+def flask_get_resistance_for_tb_sample(vcf_id):
+    return get_resistance_for_tb_sample(vcf_id)
 
 if __name__ == "__main__":
     app.run(port=8990, debug=True)
