e74a4a4e0fe549857eb7a0214c420a23e0224ac4
Fan Yang <fan.yang.oxford@gmail.com>
2019-11-21 16:47:46 +0000

    update map2coverage report

--- catweb/scripts/nfnvm_map2coverage_report_submit.py
+++ catweb/scripts/nfnvm_map2coverage_report_submit.py
@@ -15,10 +15,16 @@ files = pathlib.Path(pipeline_output_dir).glob('mapping2_Out/*_cov.png')
 
 for sample_filepath in files:
     try:
-        sample_name = str(sample_filepath.name).split('.')[0] 
+        file_name = str(sample_filepath.name).replace('_cov.png', '')
+        print('filename:'+file_name)
+        index = file_name.rfind('_')
+        print('index')
+        print(index)
+        sample_name = file_name[0:index]
+        print('samplename:'+sample_name)
 
         p = { 'pipeline_run_uuid': pipeline_run_uuid,
-              'sample_name': sample_name,
+              'sample_name': file_name,
               'sample_filepath': str(sample_filepath),
               'report_type': 'nfnvm_map2coverage_report' }
         print(p)
