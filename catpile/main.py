ec7393c4b6e5d39dbc0399a37d97b37530bf1baa
Denis Volk <denis.volk@gmail.com>
2020-03-31 11:42:55 +0000

    add endpoints for getting data from catpile

--- catpile/main.py
+++ catpile/main.py
@@ -14,6 +14,40 @@ con.execute("create table if not exists sp3_data (fetch_uuid, submission_uuid4, 
 
 app = Flask(__name__)
 
+@app.route('/get_runs_for_fetch/<fetch_uuid>')
+def get_runs_for_fetch(fetch_uuid):
+    rows = con.execute("select pipeline_run_uuid from fetch_to_run where fetch_uuid = ?", (fetch_uuid,))
+    return json.dumps([row[0] for row in rows])
+
+@app.route('/get_fetch_for_run/<pipeline_run_uuid>')
+def get_fetch_for_run(pipeline_run_uuid):
+    rows = con.execute("select fetch_uuid from fetch_to_run where pipeline_run_uuid = ?", (pipeline_run_uuid,)).fetchall()
+    if rows:
+        return json.dumps(rows[0][0])
+    else:
+        return json.dumps(None)
+
+@app.route('/get_sp3_data_for_fetch/<fetch_uuid>')
+def get_sp3_data_for_fetch(fetch_uuid):
+    con.row_factory = sqlite3.Row
+    rows = con.execute("select * from sp3_data where fetch_uuid = ?", (fetch_uuid,)).fetchall()
+    con.row_factory = None
+    return json.dumps([dict(row) for row in rows])
+
+@app.route('/fetch_to_run', methods=['POST'])
+def fetch_to_run():
+    j = request.json
+    if not 'fetch_uuid' in j or not 'pipeline_run_uuid' in j:
+        abort(400)
+    fetch_uuid = j['fetch_uuid']
+    pipeline_run_uuid = j['pipeline_run_uuid']
+
+    link_data(fetch_uuid, pipeline_run_uuid)
+
+def link_data(fetch_uuid, pipeline_run_uuid):
+    con.execute(f"insert into fetch_to_run values (?,?)", (fetch_uuid, pipeline_run_uuid))
+    con.commit()
+
 @app.route('/load_sp3_data/', methods=['POST'])
 def load_sp3_data():
     j = request.json
@@ -40,5 +74,7 @@ def add_data(fetch_uuid, fetch_dir):
 if __name__ == '__main__':
     if sys.argv[1] == 'add':
         add_data(sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == 'link':
+        link_data(sys.argv[2], sys.argv[3])
     else:
         app.run(port=22000)
