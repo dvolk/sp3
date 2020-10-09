173badeb2fc91d3a9b473b872832dade63a6fa88
Fan Yang <fan.yang.oxford@gmail.com>
2020-10-09 21:16:19 +0000

    Only call resistance api for TB samples
    Problem:
    Wasting resources on resistance report for Non-TB samples
    Although we only show TB resistance in UI, but API call remains for
    all samples
    
    Solution:
    - tb samples are identified by reference picker
    - if reference picked as NC_000962.3, consider as TB
    - it is consistent with UI now

--- catweb/scripts/clockwork_resistance_submit.py
+++ catweb/scripts/clockwork_resistance_submit.py
@@ -11,22 +11,28 @@ data = json.loads(sys.argv[1])
 pipeline_run_uuid = data['run_uuid']
 pipeline_output_dir = data['output_dir']
 
-files = pathlib.Path(pipeline_output_dir).glob('*/minos/final.vcf')
+files = list(pathlib.Path(pipeline_output_dir).glob('*/minos/final.vcf'))
+
+print(f"found {len(files)} files")
 
 for sample_filepath in files:
-    # >>> p = list(pathlib.Path.cwd().glob('*/*/final.vcf'))[0]
-    # >>> p.parts
-    # ('/', 'mnt', 'disk2', 'output', 'abf01c96-19f8-48ec-b162-fb9710d49872', 'ERR2509678', 'minos', 'final.vcf')
     try:
         sample_name = sample_filepath.parts[-3]
         pipeline_run_uuid = sample_filepath.parts[-4]
-        
+
+        with open(pathlib.Path(pipeline_output_dir) / sample_name / 'speciation' / 'reference_info.txt') as f:
+            reference_info = json.loads(f.read())
+
         p = { 'pipeline_run_uuid': pipeline_run_uuid,
               'sample_name': sample_name,
               'sample_filepath': str(sample_filepath),
               'report_type': 'resistance' }
         print(p)
-    
+
+        if reference_info['pick_taxid'] != "NC_000962.3":
+            print(f"skipping {sample_name} (wrong reference)")
+            continue
+
         r = requests.post('http://localhost:10000/req_report/', json = p)
-    except:
-        pass
+    except Exception as e:
+        print(e)
