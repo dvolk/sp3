3336e4400260907f8d1b5ed8ea84329f9588afc2
Fan Yang <fan.yang.oxford@gmail.com>
2020-10-26 13:01:45 +0000

    don't edit bloggenng h1 heading

--- sp3doc/bloggenng/main.py
+++ sp3doc/bloggenng/main.py
@@ -15,14 +15,12 @@ for post_path in post_paths:
     soup = BS(md_html, features="lxml")
     post_out = BS(header + str(soup) + footer, features="lxml")
     post_out.html.head.insert(0, BS(f'<title>{ post_out.h2.text }</title>', features="lxml").html.head.title)
-    post_out.h1.replaceWith(BS(f'<h1><a href="index.html">{ post_out.h1.text }</a></h1>', features="lxml").body.next)
     post_out.h2.replaceWith(BS(f'<h2><a href="./{ post_path.stem }.html">{ soup.h2.text }</a></h2>', features="lxml").body.next)
     out_path = f"posts/{ post_path.stem }.html"
     open(out_path, "w").write(str(post_out))
     soup.h2.replaceWith(BS(f'<h2><a href="./{ post_path.stem }.html">{ soup.h2.text }</a></h2>', features="lxml").body.next)
-    posts.append(soup.prettify())
+    posts.append(str(soup))
 
 soup = BS(header + "".join(posts) + footer, features="lxml")
 soup.html.head.insert(0, BS(f'<title>{ soup.h1.text }</title>', features="lxml").html.head.title)
-soup.h1.replaceWith(BS(f'<h1><a href="./index.html">{ soup.h1.text }</a></h1>', features="lxml").body.next)
-print(soup.prettify())
+print(str(soup))
