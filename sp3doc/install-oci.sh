f462f29051502087b83cdf4c4ada73eac756682a
Denis Volk <denis.volk@gmail.com>
2021-04-14 10:46:31 +0000

    Update install-oci.sh

--- sp3doc/install-oci.sh
+++ sp3doc/install-oci.sh
@@ -67,7 +67,7 @@ sleep 60
 ssh-keygen -b 2048 -t rsa -f /home/ubuntu/.ssh/id_rsa -q -N ""
 
 source /home/ubuntu/env/bin/activate
-pip3 install argh
+pip3 install argh waitress
 python3 /home/ubuntu/sp3/sp3doc/oracle-deploy/configure_catcloud.py /home/ubuntu/stack_info.json /home/ubuntu/sp3/catcloud/config.yaml-example > /home/ubuntu/sp3/catcloud/config.yaml
 
 systemctl --user restart catcloud-oracle
