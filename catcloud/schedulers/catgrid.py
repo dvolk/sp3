ec843c0131319c4afe56462a1d3151f16d1b64c9
Denis Volk <denis.volk@gmail.com>
2021-05-26 14:58:31 +0000

    Add node_timeout option to CatgridScheduler
    kanboard task #395
    
    Node timeout used to be hardcoded to 300 seconds. This adds a
    config.yaml option to the scheduler to set this timeout.

--- catcloud/schedulers/catgrid.py
+++ catcloud/schedulers/catgrid.py
@@ -4,8 +4,11 @@ import time
 import requests
 
 class CatgridScheduler:
-    def __init__(self):
-        pass
+    def __init__(self, node_timeout=300):
+        # time elapsed since last job after which a node will be marked as
+        # idle (and probably terminated)
+        self.node_timeout = node_timeout
+
 
     def get_info(self):
         r = requests.get('http://127.0.0.1:6000/status').json()
@@ -18,7 +21,7 @@ class CatgridScheduler:
         time_now = int(time.time())
 
         for node_name, node in nodes_running.items():
-            if not node['jobs'] and time_now - node['last_finished'] > 300:
+            if not node['jobs'] and time_now - node['last_finished'] > self.node_timeout:
                 idle_nodes.append(node_name)
 
         return len(nodes_running), len(queue), idle_nodes
