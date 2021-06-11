f7749f3fc928d72cb8a3639a739e6d3161e8aae4
Denis Volk <denis.volk@gmail.com>
2021-06-11 09:28:45 +0000

    handle systemd service check when no services have failed
    systemctl formats the services list differently if a service
    has failed or no services have failed

--- catweb/service_check.py
+++ catweb/service_check.py
@@ -5,14 +5,14 @@ import argh
 def go():
     # sp3 systemd service status
     try:
-        lines = subprocess.check_output(shlex.split("systemctl --user"), timeout=1).decode().split('\n')
+        lines = subprocess.check_output(shlex.split("systemctl --user --plain --no-pager"), timeout=1).decode().split('\n')
     except Exception as e:
         print(str(e))
         return "Service check error: couldn't get systemd service status within 1 second. This may be because of your journal size. Consider reducing your journal size."
 
     ret = list()
     for line in lines:
-        l = line[2:].split()
+        l = line.strip().split()
         if len(l) >= 5 and l[4] == 'SP3':
             ret.append({ 'name': l[0], 'status': l[3] })
     return ret
