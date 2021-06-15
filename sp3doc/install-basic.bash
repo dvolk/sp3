cdf2dc0c1133162202e5a6f461abdd9638bfab04
Denis Volk <denis.volk@gmail.com>
2021-06-15 11:11:36 +0000

    Install argh in environment immediately

--- sp3doc/install-basic.bash
+++ sp3doc/install-basic.bash
@@ -49,7 +49,7 @@ cd sp3
 # sp3 packages
 #
 pip3 install -r requirements.txt
-pip3 install waitress pymongo newick
+pip3 install waitress pymongo newick argh
 
 #
 # copy slurm emu files to /usr/bin
