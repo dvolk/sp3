a74187f5b27534163699d5330ecc43424bd6f786
Denis Volk <denis.volk@gmail.com>
2021-06-30 17:03:49 +0000

    install sentry-sdk and sentry-sdk[flask]

--- sp3doc/install-basic.bash
+++ sp3doc/install-basic.bash
@@ -49,7 +49,7 @@ cd sp3
 # sp3 packages
 #
 pip3 install -r requirements.txt
-pip3 install waitress pymongo newick argh
+pip3 install waitress pymongo newick argh sentry-sdk sentry-sdk[flask]
 
 #
 # copy slurm emu files to /usr/bin
