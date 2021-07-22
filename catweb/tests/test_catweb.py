4075a86179afa98001ab3755f888b8aeeae3a1f3
Jez Swann <Jeremy.Swann@ndm.ox.ac.uk>
2021-07-22 15:13:26 +0100

    Checking correct output directory

--- catweb/tests/test_catweb.py
+++ catweb/tests/test_catweb.py
@@ -16,19 +16,13 @@ class TestCatweb(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # May need to change profile depending on deployment environment
-        cls.profile = "-profile singularity"
         cls.sampleNames = ("aa627688-96ac-11eb-ac3f-0200170119d8","3b3ed8e6-96ac-11eb-ac3f-0200170119d8")
-
-        cls.nextflowFile = "/data/pipelines/ncov2019-artic-nf/main.nf"
-        cls.readPattern = "'*{1,2}.fastq.gz'"
-        cls.obj_path = "/data/pipelines/ncov2019-artic-nf/objStoreExample.csv"
-        cls.outputDir = "/work/output/test_catweb/"
-
-        utils.syscall('rm -rf ' + cls.outputDir + '/*')
+        cls.run_uuid = ""
+        cls.output_dir = ""
 
     # @classmethod
     # def tearDownClass(cls):
-    #     utils.syscall('rm -rf ' + cls.outputDir + '/*')
+    #     utils.syscall('rm -rf ' + cls.output_dir + '/*')
   
     def test_ncov_illumina_viridian(self):
         # e.g. cd ~/catsgo; python3 catsgo.py run-covid-illumina-objstore oxforduni-ncov2019-artic-nf-illumina /data/pipelines/ncov2019-artic-nf/objStoreExample.csv
@@ -43,7 +37,7 @@ class TestCatweb(unittest.TestCase):
         #     '--objstore', self.obj_path,
         #     '--varCaller', 'viridian',
         #     '--refmap', "'\"{}\"'",
-        #     '--outdir', self.outputDir
+        #     '--outdir', self.output_dir
         # ])
         command = 'python3 catsgo.py run-covid-illumina-objstore oxforduni-ncov2019-artic-nf-illumina /data/pipelines/ncov2019-artic-nf/objStoreExample.csv'
         completed_process = subprocess.run(command, cwd='/home/ubuntu/catsgo', shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, universal_newlines=True)
@@ -54,10 +48,11 @@ class TestCatweb(unittest.TestCase):
             raise utils.Error('Error in system call. Cannot continue')
         
         print(f"ran command {command}")
-        run_uuid = yaml.load(completed_process.stdout, Loader=yaml.SafeLoader)['run_uuid']
-        print(f"run uuid {run_uuid}")
+        self.run_uuid = yaml.load(completed_process.stdout, Loader=yaml.SafeLoader)['run_uuid']
+        self.output_dir = f"/work/output/{self.run_uuid}"
+        print(f"run uuid {self.run_uuid}")
         complete = False
-        checkCommand = f"python3 catsgo.py check-run oxforduni-ncov2019-artic-nf-illumina {run_uuid}"
+        checkCommand = f"python3 catsgo.py check-run oxforduni-ncov2019-artic-nf-illumina {self.run_uuid}"
         while not complete:
             completed_check = subprocess.run(checkCommand, cwd='/home/ubuntu/catsgo', shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, universal_newlines=True)
             if completed_check.returncode != 0:
@@ -78,20 +73,20 @@ class TestCatweb(unittest.TestCase):
         self.assertTrue(True)
 
     def test_ncov_illumina_viridian_output_has_output_folder(self):
-        self.assertTrue(os.path.exists(self.outputDir))
+        self.assertTrue(os.path.exists(self.output_dir))
 
     def test_ncov_illumina_viridian_output_has_consensus_folder(self):
-        qc_dir = os.path.join(self.outputDir, 'consensus_seqs')
+        qc_dir = os.path.join(self.output_dir, 'consensus_seqs')
         self.assertTrue(os.path.exists(qc_dir))
     
     def test_ncov_illumina_viridian_output_has_analysis_folder(self):
-        qc_dir = os.path.join(self.outputDir, 'analysis')
+        qc_dir = os.path.join(self.output_dir, 'analysis')
         self.assertTrue(os.path.exists(qc_dir))      
 
     def test_ncov_illumina_viridian_output_classification_has_human_read_list_file(self):
         for sample in self.sampleNames:
             filepath = os.path.join('consensus_seqs', sample + '.fasta')
-            outputFile = os.path.join(self.outputDir, filepath)
+            outputFile = os.path.join(self.output_dir, filepath)
             self.assertTrue(os.path.exists(outputFile))
             expectedFile = os.path.join("tests/expectedOutput/", filepath)
             expected_md5 = utils.md5(expectedFile)
