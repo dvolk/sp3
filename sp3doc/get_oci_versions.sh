3699b8a322f265e61a54635b21e63e4d17c463c3
Denis Volk <denis.volk@gmail.com>
2021-10-14 13:29:22 +0000

    Added dirty to versions if appropriate and print file location of release notes.

--- sp3doc/get_oci_versions.sh
+++ sp3doc/get_oci_versions.sh
@@ -2,12 +2,12 @@
 
 # Get SP3 version
 pushd /home/ubuntu/sp3
-SP3_VERSION=$(git describe --tags)
+SP3_VERSION=$(git describe --tags --dirty)
 popd
 
 # Get catsgo version
 pushd /home/ubuntu/catsgo
-CATSGO_VERSION=$(git describe --tags)
+CATSGO_VERSION=$(git describe --tags --dirty)
 popd
 
 # Get SARS-COV-2_environment version
@@ -15,15 +15,17 @@ SARS_ENV_VERSION=$(ls /data/images/aln2type_*.sif | awk -F_ '{print $NF}' | sed 
 
 # Get covid pipeline version
 pushd /data/pipelines/ncov2019-artic-nf
-COVID_PIPE_VERSION=$(git describe --tags)
+COVID_PIPE_VERSION=$(git describe --tags --dirty)
 popd
 
 # Get viridian pipeline version
 VIRIDIAN_VERSION=$(ls /data/images/viridian_workflow_*.img | awk -F_ '{print $NF}' | sed 's/\.[^.]*$//')
 
 mkdir -p /home/ubuntu/release_notes
+filename=/home/ubuntu/release_notes/$(date +%F_%T)_versions.txt
 echo "SP3 - ${SP3_VERSION}
 CATSGO - ${CATSGO_VERSION}
 SARS-COV-2_environments - ${SARS_ENV_VERSION}
 COVID_pipeline - ${COVID_PIPE_VERSION}
-Viridian version - ${VIRIDIAN_VERSION}" >> /home/ubuntu/release_notes/$(date +%F_%T)_versions.txt
+Viridian version - ${VIRIDIAN_VERSION}" >> $filename
+echo "release_notes available @ ${filename}"
